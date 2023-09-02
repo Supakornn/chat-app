08e55026ffb3bf6bda82ca4e80e3438d3f13ed6d
supakorn <supakorn0728@gmail.com>
2023-09-02 13:24:05 +0700

    update ts

--- app/components/input/Select.tsx
+++ app/components/input/Select.tsx
@@ -2,32 +2,37 @@
 
 import ReactSelect from "react-select";
 
+interface Option {
+    label: string;
+    value: string;
+}
+
 interface SelectProps {
-  label: string;
-  value?: string;
-  onChange: (value: Record<string, any>) => void;
-  options: Record<string, any>[];
-  disabled?: boolean;
+    label: string;
+    value?: Option | Option[] | null;
+    onChange: (value: Record<string, any>) => void;
+    options: (Option | { label: string; options: Option[] })[];
+    disabled?: boolean;
 }
 
 const Select: React.FC<SelectProps> = ({ label, value, onChange, options, disabled }) => {
-  return (
-    <div className="z-[100]">
-      <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
-      <div className="mt-2">
-        <ReactSelect
-          isDisabled={disabled}
-          value={value}
-          onChange={onChange}
-          isMulti
-          options={options}
-          menuPortalTarget={document.body}
-          styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-          classNames={{ control: () => "text-sm" }}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="z-[100]">
+            <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+            <div className="mt-2">
+                <ReactSelect
+                    isDisabled={disabled}
+                    value={value}
+                    onChange={onChange}
+                    isMulti
+                    options={options}
+                    menuPortalTarget={document.body}
+                    styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                    classNames={{ control: () => "text-sm" }}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Select;
